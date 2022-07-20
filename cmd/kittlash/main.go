a750ca5153c3420871e0ae68e075d744fbfeffd5
Jonas Aaberg <cja@lithops.se>
2022-07-20 07:21:43 +0200

    kittlash: Add save history to file
    Signed-off-by: Jonas Aaberg <cja@lithops.se>

--- cmd/kittlash/main.go
+++ cmd/kittlash/main.go
@@ -3,14 +3,48 @@ package main
 import (
 	"fmt"
 	"kittla"
+	"log"
+	"os"
+	"path/filepath"
 
+	"github.com/OpenPeeDeeP/xdg"
 	"github.com/peterh/liner"
 )
 
+func createDir(dir string) (err error) {
+
+	if stat, err := os.Stat(dir); err != nil || !stat.IsDir() {
+		err = os.MkdirAll(dir, 0755)
+	}
+	return
+}
+
 func main() {
+
+	xdgh := xdg.New("gmelchett", "kittlash")
+
+	if err := createDir(xdgh.ConfigHome()); err != nil {
+		log.Fatal("Failed creating config directory", err)
+	}
+
+	historyFile := filepath.Join(xdgh.ConfigHome(), "history.txt")
 
 	line := liner.NewLiner()
 	defer line.Close()
+
+	if f, err := os.Open(historyFile); err == nil {
+		line.ReadHistory(f)
+		f.Close()
+	}
+
+	defer func() {
+		if f, err := os.Create(historyFile); err == nil {
+			line.WriteHistory(f)
+			f.Close()
+		} else {
+			log.Fatal("Error writing history file: ", err)
+		}
+	}()
 
 	line.SetCtrlCAborts(true)
 
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 )
 
 require (
+	github.com/OpenPeeDeeP/xdg v1.0.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/mattn/go-runewidth v0.0.3 // indirect
 	golang.org/x/sys v0.0.0-20211117180635-dee7805ff2e1 // indirect
